289c460301a79326842d0ae078033d0f52dac57b
JacekWy <34398043+JacekWy@users.noreply.github.com>
2017-12-22 15:50:46 +0100

    zmianaa

--- order.php
+++ order.php
@@ -2,26 +2,29 @@
 if(isset($_POST['order']))
 {
     if(isset($_POST['Frytki'])){
+        $frytki = 0;
         $frytki = $_POST['Frytki'];
+        $iloscf = $_POST['ilosc_Frytki'];
 
     }
     if(isset($_POST['Kebab'])){
+        $kebab = 0;
         $kebab = $_POST['Kebab'];
+        $ilosck = $_POST['ilosc_kebab'];
 
     }
     if(isset($_POST['Hamburger'])){
+        $hamburger =0;
         $hamburger = $_POST['Hamburger'];
+        $ilosch = $_POST['ilosc_hamburger'];
     }
 
 }
 
-function suma($f = 0 , $k = 0, $h = 0)
-{
-    echo "Do zaplaty: ";
-    echo $f + $k + $h.' zl';
-}
-
-suma($frytki,$kebab,$hamburger);
 
 
+
+echo "Do zaplaty: ";
+echo ($frytki * $iloscf) + ($kebab * $ilosck) + ($hamburger * $ilosch);
+echo " zl";
 
