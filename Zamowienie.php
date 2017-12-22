ba08747d8c2e87cea4fcb465b005c929c98c42cc
JacekWy <34398043+JacekWy@users.noreply.github.com>
2017-12-22 16:28:46 +0100

    zmianaa

--- Zamowienie.php
+++ Zamowienie.php
@@ -8,35 +8,10 @@
 <header>
 <h1>Zamowienie</h1>
 </header>
-<div class="form">
-    <form action="order.php" method="post">
-
-        <div class="btn">
-            <input type="checkbox" name="Frytki" id="frytki" value="3.50" >
-            <label for="frytki">Frytki</label>
-            <p>3.50 zl</p>
-            <p>Ilosc:<input type="number" name="ilosc_Frytki" size="2" value="1"></p>
-        </div>
-
-        <div class="btn">
-            <input type="checkbox" name="Kebab" id="kebab" value="9.50" >
-            <label for="kebab">Kebab</label>
-            <p>9.50 zl</p>
-            <p>Ilosc:<input type="number" name="ilosc_kebab" size="2" value="1"></p>
-        </div>
-        
-        <div class="btn">
-            <input type="checkbox" name="Hamburger" id="hamburger" value="10" >
-            <label for="hamburger">Hamburger</label>
-            <p>10 zl</p>
-            <p>Ilosc:<input type="number" name="ilosc_hamburger" size="2" value="1"></p>
-        </div>
-</div>
-
-<div id="button">
-    <input type="submit" name="order" value="Zamow">
-    </form>
-</div>
+<?php
+require_once ('form.php');
+form();
+?>
 
 
 <section>
--- order.php
+++ order.php
@@ -2,29 +2,38 @@
 if(isset($_POST['order']))
 {
     if(isset($_POST['Frytki'])){
-        $frytki = 0;
         $frytki = $_POST['Frytki'];
         $iloscf = $_POST['ilosc_Frytki'];
+        $wynik1 = $frytki * $iloscf;
 
     }
     if(isset($_POST['Kebab'])){
-        $kebab = 0;
         $kebab = $_POST['Kebab'];
         $ilosck = $_POST['ilosc_kebab'];
+        $wynik2 = $kebab * $ilosck;
 
     }
     if(isset($_POST['Hamburger'])){
-        $hamburger =0;
         $hamburger = $_POST['Hamburger'];
         $ilosch = $_POST['ilosc_hamburger'];
+        $wynik3 = $hamburger * $ilosch;
+
     }
+
+
+
+}
+$ilosc = $iloscf + $ilosck + $ilosch;
+$suma = $wynik1 + $wynik2 + $wynik3;
+
+if($suma >= 70){
+    echo 'Skorzystales z promocji 15% za zakup powyzej 100zl'.'<br>';
+    $procent = $suma * 0.1;
+    $suma -=$procent;
 
 }
 
 
 
-
-echo "Do zaplaty: ";
-echo ($frytki * $iloscf) + ($kebab * $ilosck) + ($hamburger * $ilosch);
-echo " zl";
+echo "Do zaplaty: ".$suma.' zl';
 
